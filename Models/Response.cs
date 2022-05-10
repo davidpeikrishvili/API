845d15ded8773dccd6f69facdc0157585983df40
David Peikrishvili <73257917+davidpeikrishvili@users.noreply.github.com>
2022-05-10 18:09:26 -0400

    Add files via upload

---
+++ Models/Response.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+namespace AnimeAPI.Models
+{
+    public  class Response
+    {
+        [Key]
+        public  int StatusCode { get; set; }
+        public string StatusDesc { get; set; }
+        public info? info { get; set; }
+        public Theme? theme { get; set; }
+    }
+}
